7d84e98c60ea7cc783362c8e9f4765e618b84dbe
iamrajatsingh1 <iamrajatsingh1@gmail.com>
2023-12-27 00:51:56 +0530

    fix: remove actions

--- src/actions/fetchUserLists.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const fetchUserList = () => {
-  return axios
-    .get('https://jsonplaceholder.typicode.com/users')
-    .then((response) => {
-      return response.data || []
-    })
-}
