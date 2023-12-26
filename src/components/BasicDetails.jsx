7ae14b2294e619fa0023a71c065cd6368dd7c611
iamrajatsingh1 <iamrajatsingh1@gmail.com>
2023-12-27 00:48:33 +0530

    fix: prettified file

--- src/components/BasicDetails.jsx
+++ src/components/BasicDetails.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import "../styles/BasicDetails.css"
+import React from 'react'
+import '../styles/BasicDetails.css'
 
 const BasicDetails = ({ user }) => {
   return (
     <div className="user-basic-details">
       <div className="flex-container">
-        <span><strong>Name:</strong> {user?.name}</span>
         <span>
-          <strong>Address:</strong> {user?.address?.street} {user?.address?.suite}{' '}
-          {user?.address?.city}
+          <strong>Name:</strong> {user?.name}
+        </span>
+        <span>
+          <strong>Address:</strong> {user?.address?.street}{' '}
+          {user?.address?.suite} {user?.address?.city}
         </span>
       </div>
       <div className="flex-container">
-        <span><strong>Username:</strong> {user?.username}</span>
-        <span><strong>Email:</strong> {user?.email}</span>
+        <span>
+          <strong>Username:</strong> {user?.username}
+        </span>
+        <span>
+          <strong>Email:</strong> {user?.email}
+        </span>
       </div>
     </div>
   )
